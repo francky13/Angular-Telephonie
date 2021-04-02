39a40df192f57fa6ac526c31aed1ed3ba63bbfaf
hery_toavina <aherytoavina@gmail.com>
2021-04-02 12:58:08 +0300

    url

--- src/app/service/forfait.service.ts
+++ src/app/service/forfait.service.ts
@@ -1,30 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ToolsService } from './tools.service';
-import { url } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForfaitService {
-
+  url = 'https://telephonie-services.herokuapp.com/';
   constructor(private http: HttpClient, private toolsServ: ToolsService) { }
   listeForfait(){
-    return this.http.get(`${url}forfait/listeForfait`);
+    return this.http.get(`${this.url}forfait/listeForfait`);
   }
    getInsertionForfait(Insertion) {
      const options = this.toolsServ.formOption();
-     const za = this.http.post(`${url}forfait/addForfait`, Insertion, options);
+     const za = this.http.post(`${this.url}forfait/addForfait`, Insertion, options);
      console.log(Insertion);
      return za;
   }
   statistic(){
-    return this.http.get(`${url}forfait/getVenteForfait`);
+    return this.http.get(`${this.url}forfait/getVenteForfait`);
   }
 
   insertConso(Insertion) {
     const options = this.toolsServ.formOption();
-    const za = this.http.post(`${url}forfait/addConsommation`, Insertion, options);
+    const za = this.http.post(`${this.url}forfait/addConsommation`, Insertion, options);
     console.log(Insertion);
     return za;
  }
--- src/app/service/liste.service.ts
+++ src/app/service/liste.service.ts
@@ -1,23 +1,19 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {url} from 'src/environments/environment';
 import { ToolsService } from './tools.service';
-
-
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListeService {
-
+  url = 'https://telephonie-services.herokuapp.com/';
   constructor(private http: HttpClient, private toolsServ : ToolsService) { }
     getListe() {
-    return this.http.get(`${url}/depot/depotAttente`);
+    return this.http.get(`${this.url}/depot/depotAttente`);
   }
    getConfirmationDepot(input) {
      const options = this.toolsServ.formOption();
-     const za = this.http.post(`${url}/depot/confirmation`, input, options);
+     const za = this.http.post(`${this.url}/depot/confirmation`, input, options);
      console.log(za);
      return za;
   }
