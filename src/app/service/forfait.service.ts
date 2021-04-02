096554b52b3171e38d1cd9c8d082657bfc4b0257
hery_toavina <aherytoavina@gmail.com>
2021-04-02 12:40:39 +0300

    Update forfait.service.ts

--- src/app/service/forfait.service.ts
+++ src/app/service/forfait.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ToolsService } from './tools.service';
-import {url} from '../../environments/environment';
+import { url } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
